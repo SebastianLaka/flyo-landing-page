e838faa5b0176e9b2908c91b6618dc8a55b6bbdf
Sebastian <sebastianlakla@gmail.com>
2024-05-27 21:13:55 +0200

    Add pevent default and error variables with error when email is incorrect

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const headerInput = document.querySelector(".header-interaction__input");
 const newsLetterInput = document.querySelector(".newsletter-interaction__email-place");
 const headerCTA = document.querySelector('.header-interaction__cta')
 const newsLetterCTA = document.querySelector('.newsletter-interaction__cta')
-console.log(headerInput, newsLetterInput, headerCTA, newsLetterCTA);
+const errorHeader = document.querySelector(".header-interaction__error")
+const errorNewsletter = document.querySelector(".newsletter-interaction__error")
+console.log(headerInput, newsLetterInput, headerCTA, newsLetterCTA, errorHeader, errorNewsletter);
 headerCTA.addEventListener('click', e =>{
     e.preventDefault();
-    console.log("elo");
+    
 })
