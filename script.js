782aba1b53b26608d66712753efeae150be6db6d
Sebastian <sebastianlakla@gmail.com>
2024-05-27 21:02:17 +0200

    Add listener at CTA in header

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+const headerInput = document.querySelector(".header-interaction__input");
+const newsLetterInput = document.querySelector(".newsletter-interaction__email-place");
+const headerCTA = document.querySelector('.header-interaction__cta')
+const newsLetterCTA = document.querySelector('.newsletter-interaction__cta')
+console.log(headerInput, newsLetterInput, headerCTA, newsLetterCTA);
+headerCTA.addEventListener('click', e =>{
+    e.preventDefault();
+    console.log("elo");
+})
