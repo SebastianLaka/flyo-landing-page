82dca6c2b4fd7b654c324edcd5fcae63f32a5805
Sebastian <sebastianlakla@gmail.com>
2024-05-28 19:26:02 +0200

    Add event listener at CTA with prevent default and create fucntion for acrive error

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ const headerCTA = document.querySelector('.header-interaction__cta')
 const newsLetterCTA = document.querySelector('.newsletter-interaction__cta')
 const errorHeader = document.querySelector(".header-interaction__error")
 const errorNewsletter = document.querySelector(".newsletter-interaction__error")
+const inputHeader = document.querySelector('.header-interaction__input')
+const inputNewsletter = document.querySelector('.newsletter-interaction__email-place')
 console.log(headerInput, newsLetterInput, headerCTA, newsLetterCTA, errorHeader, errorNewsletter);
+const launchError = (input, error) =>{
+    input.classList.toggle('show-error');
+    error.classList.toggle('change-input-color')
+}
 headerCTA.addEventListener('click', e =>{
     e.preventDefault();
-    
+    launchError(errorHeader, inputHeader)
+})
+newsLetterCTA.addEventListener('click', e=>{
+    e.preventDefault();
+    launchError(errorNewsletter, inputNewsletter)
 })
