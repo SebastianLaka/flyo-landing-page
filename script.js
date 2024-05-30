c7944df4c73b672753d2949173c37e2212507579
Sebastian <sebastianlakla@gmail.com>
2024-05-30 22:01:55 +0200

    Add regexp for inputs when in IF condition

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 "use strict";
-const headerInput = document.querySelector(".header-interaction__input");
-const newsLetterInput = document.querySelector(".newsletter-interaction__email-place");
 const headerCTA = document.querySelector('.header-interaction__cta')
-const newsLetterCTA = document.querySelector('.newsletter-interaction__cta')
-const errorHeader = document.querySelector(".header-interaction__error")
-const errorNewsletter = document.querySelector(".newsletter-interaction__error")
 const inputHeader = document.querySelector('.header-interaction__input')
+const errorHeader = document.querySelector(".header-interaction__error")
+const newsLetterCTA = document.querySelector('.newsletter-interaction__cta')
+const errorNewsletter = document.querySelector(".newsletter-interaction__error")
 const inputNewsletter = document.querySelector('.newsletter-interaction__email-place')
-console.log(headerInput, newsLetterInput, headerCTA, newsLetterCTA, errorHeader, errorNewsletter);
+const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 const launchError = (input, error) =>{
     input.classList.toggle('show-error');
-    error.classList.toggle('change-input-color')
+    error.classList.toggle('change-input-color');
 }
 headerCTA.addEventListener('click', e =>{
     e.preventDefault();
-    launchError(errorHeader, inputHeader)
+    if(!regex.test(inputHeader.value)){
+
+        launchError(errorHeader, inputHeader) 
+    }
 })
 newsLetterCTA.addEventListener('click', e=>{
     e.preventDefault();
-    launchError(errorNewsletter, inputNewsletter)
+    if(!regex.test(inputNewsletter.value)){
+        launchError(errorNewsletter, inputNewsletter) 
+    }
 })
