8f5650a0f3e54c701a69c0e826b8c15b6810817b
Sebastian <sebastianlakla@gmail.com>
2024-06-09 20:13:54 +0200

    Add function to remove error and change names of functions. Add validation on inputs

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 "use strict";
 const headerCTA = document.querySelector('.header-interaction__cta')
-const inputHeader = document.querySelector('.header-interaction__input')
-const errorHeader = document.querySelector(".header-interaction__error")
 const newsLetterCTA = document.querySelector('.newsletter-interaction__cta')
-const errorNewsletter = document.querySelector(".newsletter-interaction__error")
+const inputHeader = document.querySelector('.header-interaction__input')
 const inputNewsletter = document.querySelector('.newsletter-interaction__email-place')
-const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-const launchError = (input, error) =>{
-    input.classList.toggle('show-error');
-    error.classList.toggle('change-input-color');
+const errorHeader = document.querySelector(".header-interaction__error")
+const errorNewsletter = document.querySelector(".newsletter-interaction__error")
+const regexEmail = /\S+@\S+\.\S+/
+const addError = (error, input) =>{
+    error.classList.add('show-error');
+    input.classList.add('change-input-color');
 }
-const regexpCheck = (emailPlace) =>{
-    if(!regex.test(emailPlace.value)){
-        const launchError = (input, error) =>{
-            input.classList.toggle('show-error');
-            error.classList.toggle('change-input-color');
-        }
-        return launchError;
+const removeError = (error, input) =>{
+    error.classList.remove('show-error');
+    input.classList.remove('change-input-color');
+}
+const checkHeaderValidation = () =>{
+    if(!regexEmail.test(inputHeader.value)){
+        addError(errorHeader, inputHeader)
+    }else if(regexEmail.test(inputHeader.value)){
+        removeError(errorHeader, inputHeader)
     }
 }
-headerCTA.addEventListener('click', e =>{
-    e.preventDefault();
-    if(regexpCheck(inputHeader)){
-        launchError(errorHeader, inputHeader) 
+const checkNewsletterValidation = () =>{
+    if(!regexEmail.test(inputNewsletter.value)){
+        addError(errorNewsletter, inputNewsletter)
+    }else if(regexEmail.test(inputNewsletter.value)){
+        removeError(errorNewsletter, inputNewsletter)
     }
-})
-newsLetterCTA.addEventListener('click', e =>{
-    e.preventDefault();
-    if(regexpCheck(inputNewsletter)){
-        launchError(errorNewsletter, inputNewsletter) 
-    }
-})
+}
+headerCTA.addEventListener('click', checkHeaderValidation)
+newsLetterCTA.addEventListener('click', checkNewsletterValidation)
